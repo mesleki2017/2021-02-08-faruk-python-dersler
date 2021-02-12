f28d92f17f9afd5251b3ef171078c567397b976e
mesleki2017 <mesleki2017@gmail.com>
2021-02-12 13:20:15 +0300

    excel veri okuma

---
+++ excel_verri_okuma_1.py
@@ -0,0 +1,10 @@
+from openpyxl import Workbook
+import openpyxl
+
+workbook = openpyxl.load_workbook("veriyazdim.xlsx")
+
+sheet = workbook.active
+
+print(sheet.cell(row=1, column=1).value)
+
+print(sheet.cell(row=2, column=1).value)
