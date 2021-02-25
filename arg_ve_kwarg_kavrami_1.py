e69f44ccf4c3a91a2b69418730a9478a6a0cd8d1
mesleki2017 <mesleki2017@gmail.com>
2021-02-25 19:47:27 +0300

    arg = argument kwargs = key word argument demek

--- arg_ve_kwarg_kavrami_1.py
+++ arg_ve_kwarg_kavrami_1.py
@@ -1,5 +1,5 @@
 
-def deneme(*aaa, **bbb):
+def arg_ve_kwarg_anlama(*aaa, **bbb):
     print(aaa[0])
     print(aaa[1])
     print(bbb["elma"])
@@ -7,7 +7,7 @@ def deneme(*aaa, **bbb):
 
 
 try:
-    deneme(1, 4, elma=55, armut=33)
+    arg_ve_kwarg_anlama(1, 4, elma=55, armut=33)
     print("1-----------------------------------")
 except:
     print("hata1")
@@ -15,42 +15,41 @@ except:
 
 
 try:
-    deneme(1, 4, 6, elma=55, armut=33)
+    arg_ve_kwarg_anlama(1, 4, 6, elma=55, armut=33)
     print("2-----------------------------------")
 except:
     print("hata2")
     print("2-----------------------------------")
 
 try:
-    deneme(1, 4, 6, kiraz=55, armut=33)
+    arg_ve_kwarg_anlama(1, 4, 6, kiraz=55, armut=33)
     print("3-----------------------------------")
 except:
     print("hata3")
     print("3-----------------------------------")
 
 try:
-    deneme(1, 4, 6, 100, "tttt", elma="var", armut="yok")
+    arg_ve_kwarg_anlama(1, 4, 6, 100, "tttt", elma="var", armut="yok")
     print("4-----------------------------------")
 except:
     print("hata4")
     print("4-----------------------------------")
 
-# https://www.w3schools.com/python/gloss_python_loop_dictionary_items.asp
 
-
-def deneme2(*zzz, **rrr):
-    for iii in zzz:
+def arg_ve_kwarg_listele(*liste_gir, **dictionary_gir):
+    for iii in liste_gir:
         print(iii)
     print("++++++++++++++++")
-    for ttt in rrr:
+    for ttt in dictionary_gir:
         print(ttt)
     print("++++++++++++++++")
-    for ttt in rrr.values():
+    for ttt in dictionary_gir.values():
         print(ttt)
     print("++++++++++++++++")
-    for xxx, yyy in rrr.items():
+    for xxx, yyy in dictionary_gir.items():
         print(xxx, " = ", yyy)
 
 
+# https://www.w3schools.com/python/gloss_python_loop_dictionary_items.asp
 print("-----------------------------------------------------")
-deneme2(12, 33, 45, "son1", elma=1, armut=2, kiraz="yok")
+arg_ve_kwarg_listele(12, 33, 45, "son1", elma=1, armut=2, kiraz="yok")
