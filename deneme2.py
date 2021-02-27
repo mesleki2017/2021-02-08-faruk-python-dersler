6d3f975886736312c0877816fca2d88089759077
mesleki2017 <mesleki2017@gmail.com>
2021-02-27 15:31:58 +0300

    ok

--- deneme2.py
+++ deneme2.py
@@ -1,56 +1,6 @@
-
-def deneme(*aaa, **bbb):
-    print(aaa[0])
-    print(aaa[1])
-    print(bbb["elma"])
-    print(bbb["armut"])
-
-
-try:
-    deneme(1, 4, elma=55, armut=33)
-    print("1-----------------------------------")
-except:
-    print("hata1")
-    print("1-----------------------------------")
-
-
-try:
-    deneme(1, 4, 6, elma=55, armut=33)
-    print("2-----------------------------------")
-except:
-    print("hata2")
-    print("2-----------------------------------")
-
-try:
-    deneme(1, 4, 6, kiraz=55, armut=33)
-    print("3-----------------------------------")
-except:
-    print("hata3")
-    print("3-----------------------------------")
-
-try:
-    deneme(1, 4, 6, 100, "tttt", elma="var", armut="yok")
-    print("4-----------------------------------")
-except:
-    print("hata4")
-    print("4-----------------------------------")
-
-# https://www.w3schools.com/python/gloss_python_loop_dictionary_items.asp
-
-
-def deneme2(*zzz, **rrr):
-    for iii in zzz:
-        print(iii)
-    print("++++++++++++++++")
-    for ttt in rrr:
-        print(ttt)
-    print("++++++++++++++++")
-    for ttt in rrr.values():
-        print(ttt)
-    print("++++++++++++++++")
-    for xxx, yyy in rrr.items():
-        print(xxx, " = ", yyy)
-
-
-print("-----------------------------------------------------")
-deneme2(12, 33, 45, "son1", elma=1, armut=2, kiraz="yok")
+import plotly.express as px
+df = px.data.gapminder().query("year == 2007")
+fig = px.scatter_geo(df, locations="iso_alpha",
+                     size="pop",  # size of markers, "pop" is one of the columns of gapminder
+                     )
+fig.show()
---
+++ matplotlib/matplotlib_liste_siralama_bar_chart_2_dahaiyi.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from numpy import random
+import numpy as np
+import matplotlib
+
+from en_buyuk_sayi1 import en_buyuk_sayiyi_bul
+
+
+liste1 = [38, 2150, 888, 787, 98, 99, 786, 200, 2000]
+liste2 = random.randint(5000, size=(200))
+
+fig = plt.figure()
+
+
+aaa = len(liste2)
+xxx = np.arange(len(liste2))
+bbb = np.zeros(len(liste2), dtype=int)
+grafik1 = plt.bar(xxx, liste2)
+
+
+def deneme1(i, listegir):
+    global aaa
+    global xxx
+    global bbb
+    if aaa == 1:
+        return
+    enBuyukSAyi = en_buyuk_sayiyi_bul(listegir, aaa)
+    if enBuyukSAyi[0] == None:
+        print("ok")
+
+    aaa = aaa-1
+
+    gecici1 = listegir[aaa]
+    listegir[aaa] = enBuyukSAyi[0]
+    listegir[enBuyukSAyi[1]] = gecici1
+
+    for i, b in enumerate(grafik1):
+        b.set_height(listegir[i])
+
+
+anim = animation.FuncAnimation(fig, deneme1, fargs=[
+                               liste2], interval=50)
+
+plt.show()
