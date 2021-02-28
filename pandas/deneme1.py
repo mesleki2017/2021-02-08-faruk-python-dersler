ccaff23f4e316fb0d0143fb70fb63bbfef8870ff
mesleki2017 <mesleki2017@gmail.com>
2021-02-28 18:49:18 +0300

    ok

--- pandas/deneme1.py
+++ pandas/deneme1.py
@@ -1,3 +1,16 @@
-for i, b in enumerate(grafik1):
-    print(i)
-    b.set_height(listegir[i])
+# import pandas library
+import pandas as pd
+
+print("eeee")
+# dictionary with list object in values
+dict111 = {
+    'isim': ['Ahmet', 'Mehmet', 'Ali', 'Fatma'],
+    'yas': [23, 21, 22, 21],
+    'universite': ['gazi', 'odtu', 'itu', 'selcuk'],
+}
+
+# creating a Dataframe object
+#df = pd.DataFrame(dict111,columns = ['Name','Age','University'], index = ['a', 'b', 'c', 'd'])
+df = pd.DataFrame(dict111)
+
+print("kkk", df)
--- pandas/deneme2.py
+++ pandas/deneme2.py
@@ -1,6 +1,1 @@
-import plotly.express as px
-df = px.data.gapminder().query("year == 2007")
-fig = px.scatter_geo(df, locations="iso_alpha",
-                     size="pop",  # size of markers, "pop" is one of the columns of gapminder
-                     )
-fig.show()
+print("khkjhkjh")
