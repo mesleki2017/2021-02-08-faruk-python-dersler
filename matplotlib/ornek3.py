fb83dc75a679cb6cdeacc812a45ff3b5a4243ff7
mesleki2017 <mesleki2017@gmail.com>
2021-03-08 15:48:23 +0300

    ok

--- matplotlib/ornek3.py
+++ matplotlib/ornek3.py
@@ -1,9 +1,9 @@
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
 def create_graph():
 	x_numbers = [1, 2, 3]
 	y_numbers = [2, 4, 6]
-	matplotlib.pyplot.plot(x_numbers, y_numbers)
-	matplotlib.pyplot.show()
+	plt.plot(x_numbers, y_numbers)
+	plt.show()
 
 create_graph()
