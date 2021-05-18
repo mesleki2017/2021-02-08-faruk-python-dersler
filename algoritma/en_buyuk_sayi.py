83dc0d676eaeaa134f1b3f50803f10bcf6f364c1
mesleki2017 <mesleki2017@gmail.com>
2021-05-18 14:54:32 +0300

    temizlik

--- algoritma/en_buyuk_sayi.py
+++
@@ -1,7 +0,0 @@
-
-def en_buyuk_sayiyi_bul(liste):
-    en_buyuk_sayi = 0
-    for i in range(0, len(liste)):
-        if liste[i] > en_buyuk_sayi:
-            en_buyuk_sayi = liste[i]
-    return en_buyuk_sayi
